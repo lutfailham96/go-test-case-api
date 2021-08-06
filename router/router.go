25c7d5ded21f2f3d36baea5ce8bd2fb71add2204
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-08-06 23:35:31 +0700

    Add user by id router

--- router/router.go
+++ router/router.go
@@ -15,4 +15,8 @@ func SetupRoutes(app *fiber.App) {
 	auth := api.Group("/auth")
 	auth.Post("/login", handler.Login)
 	auth.Post("/register", handler.CreateUser)
+
+	// User
+	user := api.Group("/user")
+	user.Get("/:id", handler.GetUser)
 }
