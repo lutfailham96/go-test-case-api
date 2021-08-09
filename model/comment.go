86a0b092df3f1ad6f52fa853cd031c7d63471992
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-08-09 14:44:58 +0700

    Refactor comment model

--- model/comment.go
+++ model/comment.go
@@ -1,10 +1,17 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Comment struct {
-	gorm.Model
-	CommentText string `gorm:"not null" json:"comment_text"`
-	ArticleID   int    `json:"article_id"`
-	UserID      int    `json:"user_id"`
+	ID          uint           `gorm:"primarykey" json:"id"`
+	CommentText string         `gorm:"not null" json:"comment_text"`
+	ArticleID   uint           `gorm:"not null" json:"article_id"`
+	UserID      uint           `gorm:"not null" json:"user_id"`
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   time.Time      `json:"updated_at"`
+	DeletedAt   gorm.DeletedAt `gorm:"index" json:"deleted_at"`
 }
