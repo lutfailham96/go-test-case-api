6273b1a23a30a306de74885eaa8fcd2f48c9a60e
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-08-09 14:42:45 +0700

    Refactor article model

--- model/article.go
+++ model/article.go
@@ -1,12 +1,19 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Article struct {
-	gorm.Model
-	Title            string    `gorm:"not null" json:"title"`
-	Content          string    `gorm:"not null" json:"content"`
-	FeaturedImageUrl string    `gorm:"not null" json:"featured_image_url"`
-	UserID           int       `json:"user_id"`
-	Comments         []Comment `gorm:"-" json:"comments"`
+	ID               uint           `gorm:"primarykey" json:"id"`
+	Title            string         `gorm:"not null" json:"title"`
+	Content          string         `gorm:"not null" json:"content"`
+	FeaturedImageUrl string         `gorm:"not null" json:"featured_image_url"`
+	UserID           uint           `gorm:"not null" json:"user_id"`
+	Comments         []Comment      `json:"comments"`
+	CreatedAt        time.Time      `json:"created_at"`
+	UpdatedAt        time.Time      `json:"updated_at"`
+	DeletedAt        gorm.DeletedAt `gorm:"index" json:"deleted_at"`
 }
