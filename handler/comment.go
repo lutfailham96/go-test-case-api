b6a54e6f8ee5818fb50a11563ac04580b00fae14
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-08-09 22:12:24 +0700

    Add delete comment handler

--- handler/comment.go
+++ handler/comment.go
@@ -16,3 +16,17 @@ func CreateComment(c *fiber.Ctx) error {
 	db.Create(&comment)
 	return c.Status(fiber.StatusCreated).JSON(fiber.Map{"status": "success", "message": "Created comment", "data": comment})
 }
+
+func DeleteComment(c *fiber.Ctx) error {
+	id := c.Params("id")
+	db := database.DB
+
+	var comment model.Comment
+	db.Find(&comment, id)
+
+	if comment.ID == 0 {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"status": "error", "message": "No comment found with ID", "data": nil})
+	}
+	db.Delete(&comment)
+	return c.Status(fiber.StatusGone).JSON(fiber.Map{"status": "success", "message": "Comment deleted", "data": nil})
+}
