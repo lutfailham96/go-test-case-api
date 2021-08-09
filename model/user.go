73855b3a79abb37768321eb48cd5ede2394eebb3
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2021-08-09 14:46:22 +0700

    Refactor user model

--- model/user.go
+++ model/user.go
@@ -1,16 +1,23 @@
 package model
 
-import "github.com/jinzhu/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
-	gorm.Model
-	Email     string    `gorm:"unique_index;not null" json:"email"`
-	Username  string    `gorm:"unique_index;not null" json:"username"`
-	Password  string    `gorm:"not null" json:"password"`
-	Name      string    `gorm:"not null" json:"name"`
-	Address   string    `gorm:"not null" json:"address"`
-	AvatarUrl string    `json:"avatar_url"`
-	Role      string    `gorm:"not null" json:"role"`
-	Articles  []Article `gorm:"-" json:"articles"`
-	Comments  []Comment `gorm:"-" json:"comments"`
+	ID        uint           `gorm:"primarykey" json:"id"`
+	Email     string         `gorm:"unique_index;not null" json:"email"`
+	Username  string         `gorm:"unique_index;not null" json:"username"`
+	Password  string         `gorm:"not null" json:"password"`
+	Name      string         `gorm:"not null" json:"name"`
+	Address   string         `gorm:"not null" json:"address"`
+	AvatarUrl string         `json:"avatar_url"`
+	Role      string         `gorm:"not null" json:"role"`
+	Articles  []Article      `json:"articles"`
+	Comments  []Comment      `json:"comments"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
 }
